2277335fe84857aabad1290f63c81e71d8beff63
Forevercpu <69519383+Forevercpu@users.noreply.github.com>
2026-07-17 15:02:04 +0800

    配置管理后台端口为3719

--- aistock-admin-fe/vite.config.ts
+++ aistock-admin-fe/vite.config.ts
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3719,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3717',
         changeOrigin: true,
       },
     },
   },
 });
-
