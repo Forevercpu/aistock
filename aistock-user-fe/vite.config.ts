172002e74da1e24c1af4d63261517abc1712311b
Forevercpu <69519383+Forevercpu@users.noreply.github.com>
2026-07-17 15:02:12 +0800

    配置用户端端口为3718

--- aistock-user-fe/vite.config.ts
+++ aistock-user-fe/vite.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-export default defineConfig({ plugins: [react()] });
-
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3718,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3717',
+        changeOrigin: true,
+      },
+    },
+  },
+});
