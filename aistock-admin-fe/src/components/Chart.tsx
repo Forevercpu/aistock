9294d7484c63ddd856da0c155dfeb9caa8f81caf
Forevercpu <69519383+Forevercpu@users.noreply.github.com>
2026-07-19 16:10:30 +0800

    修复数据总览图表组件加载异常

--- aistock-admin-fe/src/components/Chart.tsx
+++ aistock-admin-fe/src/components/Chart.tsx
@@ -1,12 +1,6 @@
-import ReactEChartsCore from 'echarts-for-react/lib/core';
-import * as echarts from 'echarts/core';
-import { BarChart, LineChart, PieChart } from 'echarts/charts';
-import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components';
-import { CanvasRenderer } from 'echarts/renderers';
+import ReactECharts from 'echarts-for-react';
 import type { EChartsOption } from 'echarts';
 
-echarts.use([BarChart, LineChart, PieChart, GridComponent, LegendComponent, TooltipComponent, CanvasRenderer]);
-
 export function Chart({ option, height = 280 }: { option: unknown; height?: number }) {
-  return <ReactEChartsCore echarts={echarts} option={option as EChartsOption} style={{ height }} />;
+  return <ReactECharts option={option as EChartsOption} style={{ height }} />;
 }
